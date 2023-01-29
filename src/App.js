0017c3497f0077c764345c92b9e884f592acdf3a
chikane mashaba <playtunes100@gmail.com>
2023-01-29 19:30:45 +0200

    - DeviceOrientationControls

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import { useRef, useState } from 'react'
 import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls} from '@react-three/drei'
+import { OrbitControls, DeviceOrientationControls} from '@react-three/drei'
 
 
 
@@ -35,8 +35,8 @@ function App() {
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <Box position={[0, 0, 0]} />
-      
       <OrbitControls autoRotate={true} autoRotateSpeed={0.5} />
+      <DeviceOrientationControls />
     </Canvas>
 
   );
