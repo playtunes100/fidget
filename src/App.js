c50b5c29d7940f39fae8c2467c4de885afba398d
chikane mashaba <playtunes100@gmail.com>
2023-01-31 18:53:07 +0200

    -implement scroll

--- src/App.js
+++ src/App.js
@@ -1,59 +1,69 @@
 
 import './App.css';
 import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, DeviceOrientationControls} from '@react-three/drei'
+import { Canvas, useThree} from '@react-three/fiber'
+import { Sky, Image,Text, ScrollControls, Scroll, useScroll} from '@react-three/drei'
 
-
-
-function Box(props) {
+function Item({ index, position, scale, ...props }){
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
   // Hold state for hovered and clicked events
   const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
   
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  //useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
+
+  
+  
+
   return (
-    <mesh
-      {...props}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[3, 3, 3]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
+  <Image ref={ref} {...props} position={position} scale={scale} onClick={click} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} />
+  
+  
   )
 }
 
 
-function App() {
+function Items({ w = 4, gap = 6 }) {
+  const [images, setImages] = useState(["https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/82449134_1174425096234384_2141610640999972864_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CFOKesJgq6AAX9xAhDM&_nc_ht=scontent-jnb1-1.xx&oh=00_AfBaJeGkoL8mBdszkQuTYu3spV5x3CVwtiWgnEVtmmo4EQ&oe=64009853", "https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/82449134_1174425096234384_2141610640999972864_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CFOKesJgq6AAX9xAhDM&_nc_ht=scontent-jnb1-1.xx&oh=00_AfBaJeGkoL8mBdszkQuTYu3spV5x3CVwtiWgnEVtmmo4EQ&oe=64009853", "https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/82449134_1174425096234384_2141610640999972864_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CFOKesJgq6AAX9xAhDM&_nc_ht=scontent-jnb1-1.xx&oh=00_AfBaJeGkoL8mBdszkQuTYu3spV5x3CVwtiWgnEVtmmo4EQ&oe=64009853", "https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/82449134_1174425096234384_2141610640999972864_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CFOKesJgq6AAX9xAhDM&_nc_ht=scontent-jnb1-1.xx&oh=00_AfBaJeGkoL8mBdszkQuTYu3spV5x3CVwtiWgnEVtmmo4EQ&oe=64009853"])
+  const { width } = useThree((device) => device.viewport)
+  const xW = w + gap
+  return (
+    <ScrollControls horizontal damping={1} pages={(width - xW + images.length * xW) / width}>
+      <Scroll>
+        {images.map((url, i) => {
+        
+        return(
+          <group key={'group-'+i}>
+              <Item key={i} index={i} position={[i * xW, 0, 0]} scale={[w, 4, 1]} url={url} />
+              <Text key={'text-'+i} index={i} position={[i * xW, -2.5, 0]} fontSize={0.5} color={'black'}>Welcome</Text>
+          </group>
+        
+        
+        )})}
 
-  dcontrols = new DeviceOrientationControls ( camera, renderer.domElement );
+        
+      </Scroll>
+    </ScrollControls>
+  )
+}
 
-  instructions.addEventListener( 'touchstart', function (evt) {
-				
-                dcontrols.enabled = true;
-                evt.preventDefault();
-				console.log ('touch test');
-                dcontrols.enabled = true;
-                instructions.style.display = 'none';
-                blocker.style.display = 'none';
 
-			}, false );
+function App() {       
 
   return (
     
     <Canvas>
+      <Sky distance={450} sunPosition={[0, 1, 0]} inclination={0} azimuth={180} />
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
-      <Box position={[0, 0, 0]} />
-      <OrbitControls autoRotate={true} autoRotateSpeed={0.5} />
-      <DeviceOrientationControls />
+      <Items/>
+      
     </Canvas>
 
   );
 }
 
 export default App;
+
+
