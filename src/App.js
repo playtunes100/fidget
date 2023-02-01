bdd0c17dd5ac2e75b76d31d20464d6798e722d98
chikane mashaba <playtunes100@gmail.com>
2023-02-01 16:47:23 +0200

    -Add media

--- src/App.js
+++ src/App.js
@@ -2,23 +2,19 @@
 import './App.css';
 import { useRef, useState } from 'react'
 import { Canvas, useThree} from '@react-three/fiber'
-import { Sky, Image,Text, ScrollControls, Scroll, useScroll, OrbitControls} from '@react-three/drei'
-
+import { Sky, PositionalAudio, Image, Text, ScrollControls, Scroll} from '@react-three/drei'
+import spinner from './assets/images/spinner.png'
+import pop from './assets/images/pop-it.png'
+import key from './assets/images/key.png'
+import naked from './assets/images/naked.png'
+import forest from './assets/sounds/forest.ogg'
 function Item({ index, position, scale, ...props }){
   // This reference gives us direct access to the THREE.Mesh object
   const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-
-  
-  
-
-  
   
 
   return (
-  <Image ref={ref} {...props} position={position} scale={scale} onClick={(event) => ref.current.material.grayscale = 1} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} />
+  <Image ref={ref} {...props} position={position} scale={scale} onClick={(event) => ref.current.material.grayscale = 1} onPointerOver={(event) => ref.current.material.grayscale = 1} onPointerOut={(event) => ref.current.material.grayscale = 0} />
   
   
   )
@@ -26,19 +22,49 @@ function Item({ index, position, scale, ...props }){
 
 
 function Items({ w = 4, gap = 4 }) {
-  const [images, setImages] = useState(["https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/82449134_1174425096234384_2141610640999972864_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CFOKesJgq6AAX9xAhDM&_nc_ht=scontent-jnb1-1.xx&oh=00_AfBaJeGkoL8mBdszkQuTYu3spV5x3CVwtiWgnEVtmmo4EQ&oe=64009853", "https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/82449134_1174425096234384_2141610640999972864_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CFOKesJgq6AAX9xAhDM&_nc_ht=scontent-jnb1-1.xx&oh=00_AfBaJeGkoL8mBdszkQuTYu3spV5x3CVwtiWgnEVtmmo4EQ&oe=64009853", "https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/82449134_1174425096234384_2141610640999972864_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CFOKesJgq6AAX9xAhDM&_nc_ht=scontent-jnb1-1.xx&oh=00_AfBaJeGkoL8mBdszkQuTYu3spV5x3CVwtiWgnEVtmmo4EQ&oe=64009853", "https://scontent-jnb1-1.xx.fbcdn.net/v/t1.6435-9/82449134_1174425096234384_2141610640999972864_n.png?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=CFOKesJgq6AAX9xAhDM&_nc_ht=scontent-jnb1-1.xx&oh=00_AfBaJeGkoL8mBdszkQuTYu3spV5x3CVwtiWgnEVtmmo4EQ&oe=64009853"])
+
+
+ 
+  const [images, setImages] = useState([
+  {
+    id: 1,
+    src: spinner,
+    url: "/spinner",
+    desc: "Spinner",
+  },
+  {
+    id: 2,
+    src: pop,
+    url: "/pop",
+    desc: "Pop It",
+  },
+  {
+    id: 3,
+    src: key,
+    url: "/keyboard",
+    desc: "Mechanical Keyboard",
+  },
+  {
+    id: 4,
+    src: naked,
+    url: "/naked",
+    desc: "Interactive Naked Insurance ad",
+  }
+  ,
+])
   const { width } = useThree((device) => device.viewport)
-  console.log(width)
+  
   const xW = w + gap
   return (
     <ScrollControls horizontal damping={1} pages={(width - xW + images.length * xW) / width}>
+      
       <Scroll>
         {images.map((url, i) => {
         
         return(
           <group key={'group-'+i}>
-              <Item key={i} index={i} position={[i * xW, 0, 0]} scale={[(width <= 4.80 ? (width * 0.7) : w), (width <= 4.80 ? (width * 0.7) : 4), 1]} url={url} />
-              <Text key={'text-'+i} index={i} position={[i * xW, -2.5, 0]} fontSize={0.5} color={'black'}>Welcome</Text>
+              <Item key={i} index={i} transparent position={[i * xW, 0, 0]} scale={[(width <= 4.80 ? (width * 0.7) : w), (width <= 4.80 ? (width * 0.7) : 4), 1]} url={url.src} />
+              <Text key={'text-'+i} index={i} position={[i * xW, -2.5, 0]} fontSize={0.5} color={'black'}>{url.desc}</Text>
           </group>
         
         
@@ -56,11 +82,10 @@ function App() {
   return (
     
     <Canvas>
-      <Sky distance={45} elevation={1.2} sunPosition={[0, 45, 0]} inclination={-0.001} azimuth={180} />
+      <Sky distance={80} elevation={1.2} sunPosition={[0, 45, 0]} inclination={-0.001} azimuth={180} />
       
       <Items/>
-      
-      
+      <PositionalAudio url={forest} autoplay= {true} playbackRate={1} loop distance={1}/>
     </Canvas>
 
   );
